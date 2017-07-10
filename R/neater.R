f361a8ebdead89247fb3b703cd58c1ac1b07e429
ncordon <nacho.cordon.castillo@gmail.com>
2017-07-10 03:42:52 +0200

    NEATER almost completed

--- R/neater.R
+++ R/neater.R
@@ -1,14 +1,20 @@
 # dataset and new.samples must have the same class name attribute appended
-neater <- function(dataset, new.samples, k, n.iter, class.attr){
+# new.samples <- smotefamily::SMOTE(iris[,-5], iris[,5])$syn_data
+# dataset <- iris
+# new.samples$Species <- new.samples$class
+# new.samples <- new.samples[, names(new.samples)!="class"]
+
+neater <- function(dataset, new.samples, class.attr, k, n.iter, smooth.parameter){
   n.old <- nrow(dataset)
   minority.class <- new.samples[1, class.attr]
   classes <- dataset[, class.attr]
-  minority.indexes <- which(classes != class.attr)
+  minority.indexes <- which(classes == minority.class)
 
   dataset <- rbind(dataset, new.samples)
+  dataset <- dataset[, names(dataset) != class.attr]
+  new.samples <- new.samples[, names(new.samples) != class.attr]
   knn.indexes <- FNN::knnx.index(dataset, new.samples, k = k)
   # Matrix of probabilities of belonging to each class, first -> minority, second -> not minority
-
 
   probs <- matrix(nrow = nrow(dataset), ncol = 2)
   probs[ minority.indexes, 1] <- 1
@@ -17,21 +23,38 @@ neater <- function(dataset, new.samples, k, n.iter, class.attr){
   probs[-minority.indexes, 2] <- 1
   probs[(n.old + 1):nrow(dataset), ] <- 0.5
 
-
   # List with the payoffs for each synthetic sample respect to its k-nearest neighbours
-  partial.payoffs <- lapply(1:nrow(new.samples), function(index){
-    sapply(knn.indexes[index, ], function(j){
-      diff <- dataset[j, ] - dataset[index, ]
-      1 / sum(diff * diff)
+  partial.payoffs <- lapply(1:nrow(new.samples), function(i){
+    sapply(knn.indexes[i, ], function(j){
+      diff <- dataset[j, ] - dataset[i, ]
+      1 / (sum(diff * diff) + 1)
     })
   })
 
+  for(i in 1:n.iter){
+    payoffs <- sapply(1:nrow(new.samples), function(i){
+      # Calculate payoff for ith tuple
+      sum( partial.payoffs[[i]] * (probs[knn.indexes[i, ], ] %*% probs[n.old + i, ]) )
+    })
 
-  lapply(1:nrow(new.samples), function(index){
-    # Calculate payoff for index tuple
-    sum( sapply(knn.indexes[index, ], function(j){
-      partial.payoffs[[i]][j] * as.numeric(probs[i] * probs[j])
-    }) )
-  })
+    avg.payoff <- mean(payoffs)
 
+    updated.probs <- lapply(1:length(payoffs), function(i){
+      min.prob <- probs[n.old + i,1] * (smooth.parameter + payoffs[i]) / (smooth.parameter + avg.payoff)
+      min.prob <- min(1, min.prob)
+      c(min.prob, 1-min.prob)
+    })
+
+    # Updates probabilities of each class
+    updated.probs <- do.call(rbind, updated.probs)
+    probs <- rbind(probs[1:n.old, ], updated.probs)
+  }
+
+  # Select synthetic instances whose probabily of belonging to the minority class is greater than a half
+  good.syn.samples <-  which(probs[(n.old+1):nrow(probs), 1] > 0.5)
+  new.samples <- new.samples[good.syn.samples, ]
+  # Append class column to minority samples and return them
+  new.samples[, class.attr] <- minority.class
+
+  new.samples
 }
