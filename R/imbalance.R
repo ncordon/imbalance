dd27258b5aaaa1e9da0c20d5301f146d1a6fe5f0
ncordon <nacho.cordon.castillo@gmail.com>
2017-11-27 23:36:43 +0100

    Adds some methods of smotefamily to the wrapper

--- R/imbalance.R
+++ R/imbalance.R
@@ -21,52 +21,117 @@ NULL
 NULL
 
 
-#' #' Wrapper to ease the class balancing preprocessing task for binary class
-#' #' \code{dataset}
-#' #'
-#' #' @param dataset A binary class \code{data.frame} to balance.
-#' #' @param ratio Numeric greater than 1.
-#' #' @param method A \code{character} corresponding to method to apply. Possible
-#' #'   methods are: \code{\link{racog}}, \code{\link{wracog}}, \code{\link{rwo}},
-#' #'   \code{\link{pdfos}}.
-#' #' @param filter Logical wheter to apply filtering of oversampled instances.
-#' #' @param classAttr String. Indicates the class attribute from \code{dataset}.
-#' #'   Must exist in it.
-#' #'
-#' #' @return \code{data.frame} with balanced classes.
-#' #' @export
-#' #'
-#' #' @examples
-#' balanceDataset <- function(dataset, ratio, method, filter = FALSE,
-#'                            classAttr = "class"){
-#'   if(!is.data.frame(dataset))
-#'     stop("dataset must be a data.frame")
-#'   if (!classAttr %in% names(dataset))
-#'     stop(paste(classAttr, "attribute not found in dataset"))
+#' Wrapper that encapsulates a collection of algorithms to perform a class
+#' balancing preprocessing task for binary class datasets
 #'
+#' @param dataset A binary class \code{data.frame} to balance.
+#' @param ratio Number between 0 and 1 indicating the desired ratio between
+#'   minority examples and majority ones, that is, the quotient
+#'   \eqn{\frac{|minority examples|}{|majority examples|}}
+#' @param method A \code{character} corresponding to method to apply. Possible
+#'   methods are: \code{\link{racog}}, \code{\link{wracog}}, \code{\link{rwo}},
+#'   \code{\link{pdfos}}, \code{\link{mwmote}},
+#'   \code{\link[smotefamily]{ADASYN}}, \code{\link[smotefamily]{ANS}},
+#'   \code{\link[smotefamily]{SMOTE}},
+#'   \code{\link[smotefamily]{Borderline-SMOTE}},
+#'   \code{\link[smotefamily]{DBSMOTE}}, \code{\link[smotefamily]{SLS}},
+#'   \code{\link[smotefamily]{RSLS}}
+#' @param filtering Logical (TRUE or FALSE) indicating wheter to apply filtering
+#'   of oversampled instances with \code{\link{NEATER}} algorithm.
+#' @param classAttr String. Indicates the class attribute from \code{dataset}.
+#'   Must exist in it.
 #'
-#'   classes <- unique(dataset[, classAttr])
-#'   classes.counts <- sapply(classes, function(c){
-#'     length(which(dataset[, classAttr] == c))
-#'   })
-#'   minority.class <- classes[which.min(classes.counts)]
+#' @return A balanced \code{data.frame} with same structure as \code{dataset},
+#'   containing both original instances and new ones
+#' @export
 #'
-#'   minority <- dataset[dataset[, classAttr] == minority.class, ]
-#'   minority.size <- nrow(minority)
-#'
-#'   # Delete class attribute
-#'   minority <- minority[, names(minority) != classAttr]
-#'
-#'
-#'   if (method == "pdfos"){
-#'     n.instances <- minority.size * ratio
-#'     new.samples <- pdfos(minority, n.instances)
-#'     names(new.samples) <- names(minority)
-#'     # Add minority class attribute and bind new instances
-#'     new.samples[ classAttr ] <- minority.class
-#'     result <- rbind(dataset, new.samples)
-#'     rownames(result) <- 1:nrow(result)
-#'   }
-#'
-#'   result
-#' }
+#' @examples
+resample <- function(dataset, ratio = NA, method = c("racog", "wracog",
+                     "pdfos", "rwo", "mwmote", "ADASYN", "ANS", "SMOTE",
+                     "Borderline-SMOTE", "DBSMOTE", "SLS", "RSLS"),
+                     filtering = FALSE, classAttr = "Class",
+                     wrapper = NA, ...){
+  checkDataset(dataset, "dataset")
+  checkDatasetClass(dataset, classAttr, "dataset")
+  originalShape <- datasetStructure(dataset, classAttr)
+
+  minority <- selectMinority(dataset, classAttr)
+  minoritySize <- nrow(minority)
+  majoritySize <- nrow(dataset) - minoritySize
+  currentRatio <- minoritySize / majoritySize
+  dupSize <- ceiling(ratio / currentRatio - 1)
+  classIndex <- which(names(dataset) == classAttr)
+
+  if(method %in% c("racog", "pdfos", "rwo", "mwmote")){
+    # Checks
+    if(is.na(ratio))
+      stop("ratio cannot be undefined for the selected method")
+    if(ratio <= 0 || ratio > 1)
+      stop("ratio must be a real number between 0 and 1")
+
+    if(ratio < currentRatio)
+      stop("ratio must be greater than current ratio of imbalance to perform
+          an oversampling")
+
+    # Compute number of required synthetic positive instances
+    numInstances <- majoritySize * ratio - minoritySize
+
+    # Evaluate method
+    selectedMethod <- eval(as.name(method))
+    newSamples <- selectedMethod(dataset, numInstances, classAttr)
+  } else if(method == "wracog"){
+      if(is.na(wrapper))
+        stop("wrapper argument not found")
+      if(! wrapper %in% c("C5.0", "Knn"))
+        stop("Possible values when using wracog are: C5.0, knn")
+
+      if(wrapper == "C5.0"){
+        myWrapper <- structure(list(), class="C50Wrapper")
+        trainWrapper.C50Wrapper <- function(wrapper, train, trainClass){
+          C50::C5.0(train, trainClass, ...)
+        }
+      } else{
+        if(wrapper == "Knn"){
+          myWrapper <- structure(list(), class = "KNNWrapper")
+          predict.KNN <- function(model, test){
+            FNN::knn(model$train, test, model$trainClass, ...)
+          }
+          trainWrapper.KNNWrapper <- function(wrapper, train, trainClass){
+            myKNN <- structure(list(), class = "KNN")
+            myKNN$train <- train
+            myKNN$trainClass <- trainClass
+            myKNN
+          }
+        }
+      }
+      trainfold <- sample(1:nrow(dataset), nrow(dataset)/2, FALSE)
+      newSamples <- wracog(dataset[trainFold, ], dataset[-trainFold, ],
+                           myWrapper, classAttr)
+  } else if(method == "ADASYN"){
+    newSamples <- smotefamily::ADAS(dataset[, -classIndex],
+                                    dataset[, classIndex])
+    newSamples <- newSamples$syn_data
+    newSamples <- newSamples[, -ncol(newSamples)]
+    newSamples <- normalizeNewSamples(originalShape, newSamples)
+  } else if(method == "ANS"){
+    newSamples <- smotefamily::ANS(dataset[, -classIndex],
+                                   dataset[, classIndex],
+                                   dupSize)
+    newSamples <- newSamples$syn_data
+    newSamples <- newSamples[, -ncol(newSamples)]
+    newSamples <- normalizeNewSamples(originalShape, newSamples)
+  } else if(method == "smotefamily"){
+    newSamples <- smotefamily::ANS(dataset[, -classIndex],
+                                   dataset[, classIndex],
+                                   dupSize)
+    newSamples <- newSamples$syn_data
+    newSamples <- newSamples[, -ncol(newSamples)]
+    newSamples <- normalizeNewSamples(originalShape, newSamples)
+  }
+
+  if(filtering)
+    newSamples <- neater(dataset, newSamples)
+
+  result <- rbind(dataset, newSamples)
+  result
+}
